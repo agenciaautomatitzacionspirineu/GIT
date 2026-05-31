919ced80c84f94ec9126107ca0bd61052dff0719
Eduard Belmonte <eduard@aap.cat>
2026-05-31 17:47:51 +0200

    Primera versio

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+   
+    <title>Registre Professional -</title>
+
+</head>
+<body>
+
+                    
+                        <p>Hola</p>
+       
+</body>
+</html>
